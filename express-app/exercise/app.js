dc149857decdd05a555405e7cc6bcff7c410c681
joel <joellucana02@gmail.com>
2022-08-12 23:50:40 -0300

    express shop list exercise

--- express-app/exercise/app.js
+++ express-app/exercise/app.js
@@ -1,3 +1,16 @@
 /* { Express.js Exercises. }
 
 For this exercise we will be building a simple application where we will store a shopping list. You should use an array to store your items in the shopping list. */
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const router = require("./routes/router");
+app.use(bodyParser.json());
+app.use(router);
+app.get("/", (req, res) => {
+  return res.json({ message: "go to /items" });
+});
+
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
--- express-app/exercise/routes/router.js
+++ express-app/exercise/routes/router.js
@@ -9,3 +9,43 @@ GET /items/:id - this route should display a single item's name and price
 PATCH /items/:id - this route should accept edits to existing items.
 
 DELETE /items/:id - this route should allow you to delete a specific item from the array. */
+
+const express = require("express");
+const app = express();
+const router = express.Router();
+const list = [];
+let id = 0;
+router.get("/items", (req, res) => {
+  return res.json({ message: "shopping items", list });
+});
+router.get("/items/:id", (req, res) => {
+  let item = list.find((e) => e.id === Number(req.params.id));
+  return res.json({ message: "item", item });
+});
+router.post("/items", (req, res) => {
+  list.push({
+    id: id++,
+    name: req.body.name,
+    price: req.body.price,
+  });
+  return res.json({ message: "data added successfully" });
+});
+router.put("/items/:id", (req, res) => {
+  let item = list.find((e) => e.id === Number(req.params.id));
+  item.name = req.body.name;
+  item.price = req.body.price;
+  return res.json({ message: "data updated successfully" });
+});
+router.delete("/items/:id", (req, res) => {
+  let itemIndex = list.indexOf(
+    list.find((e) => e.id === Number(req.params.id))
+  );
+  console.log(itemIndex);
+  if (itemIndex != -1) {
+    list.splice(itemIndex, 1);
+    return res.json({ message: "data deleted successfully" });
+  } else {
+    return res.json({ message: "index not found" });
+  }
+});
+module.exports = router;
